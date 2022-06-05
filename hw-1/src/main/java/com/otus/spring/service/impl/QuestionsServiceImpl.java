46d723fb3ee5afb9d4f4b2781e73c8d1fe7e69fa
dusova <ds.usova@inbox.ru>
2022-06-05 17:06:22 +0300

    Add service layer

---
+++ hw-1/src/main/java/com/otus/spring/service/impl/QuestionsServiceImpl.java
@@ -0,0 +1,22 @@
+package com.otus.spring.service.impl;
+
+import com.otus.spring.dao.api.QuestionsDao;
+import com.otus.spring.model.Question;
+import com.otus.spring.service.api.QuestionsService;
+
+import java.util.List;
+
+public class QuestionsServiceImpl implements QuestionsService {
+
+    private final QuestionsDao questionsDao;
+
+    public QuestionsServiceImpl(QuestionsDao questionsDao) {
+        this.questionsDao = questionsDao;
+    }
+
+    @Override
+    public List<Question> getQuestions() {
+        return questionsDao.readQuestions();
+    }
+
+}
